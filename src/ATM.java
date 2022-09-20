23d54085a8720cd53094e8aa09e513f634ef2164
pdychinco <prdychinco@gmail.com>
2022-09-20 15:15:33 -0700

    Edited functionality to properly keep user logged in after initial pin validation

--- src/ATM.java
+++ src/ATM.java
@@ -3,60 +3,85 @@ import java.util.Scanner;
 public class ATM {
   private String name;
   private ArrayList<Users> user = new ArrayList<>();
-  private Scanner scan = new Scanner(System.in);
+  private Users currentUser;
+  private final Scanner scan = new Scanner(System.in);
+  private boolean loggedIn = false;
+  public boolean isLoggedIn() {
+    return loggedIn;
+  }
+  public void setLoggedIn(boolean loggedIn) {
+    this.loggedIn = loggedIn;
+  }
   public String getName() {
     return name;
   }
-  
   public void setName(String name) {
     this.name = name;
   }
-  
+  public Users getCurrentUser() {
+    return currentUser;
+  }
+  public void setCurrentUser(Users currentUser) {
+    this.currentUser = currentUser;
+  }
   ATM(String name) {
     this.setName(name);
   }
   
   /*
-  *do stuff here to control all the atm menu functions
-  * also make a ATM.start()
+   *do stuff here to control all the atm menu functions
+   * also make a ATM.start()
    */
   public void start() {
     System.out.println("Start of ATM Script");
     System.out.println("-------------------------------------------");
     System.out.println("Welcome to Bank of Java.");
     boolean runTime = true;
-    while(runTime) {
-      String userChoice = this.startMenu();
-      switch (userChoice) {
-        case "1" -> this.login();
-        case "2" -> this.createUser();
-        case "q" -> {
-          System.out.println("Thanks for banking at Bank of Java! See you soon.");
-          runTime = false;
+    while (runTime) {
+      if (this.isLoggedIn()) {
+        this.accountPage();
+      } else {
+        String userChoice = this.startMenu();
+        switch (userChoice) {
+          case "1" -> this.login();
+          case "2" -> this.createUser();
+          case "q" -> {
+            System.out.println("Thanks for banking at Bank of Java! See you soon.");
+            runTime = false;
+          }
         }
       }
     }
-    
   }
+  
   public void login() {
     int counter = 0;
     do {
+      if (user.isEmpty()) {
+        System.out.println("No current accounts in ATM!\n");
+        break;
+      } else if (counter == 3) {
+        break;
+      } else if (this.isLoggedIn()) {
+        this.accountPage();
+      }
       System.out.println("Please enter the pin associated with your account.");
       String enteredPin = scan.next();
+  
       for (Users user : user) {
         if (enteredPin.equalsIgnoreCase(user.getPin())) {
+          this.setLoggedIn(true);
+          this.setCurrentUser(user);
           System.out.println("Login Successful!");
-          counter = 3;
+          this.accountPage();
+        } else {
+          counter += 1;
+          System.out.printf("Incorrect Pin (%d, 3)%n", counter);
         }
       }
-      if(counter >= 3) {
-        break;
-      } else {
-        System.out.println("Incorrect Pin");
-        counter += 1;
-      }
-    }while(counter <= 2);
+    } while (!this.isLoggedIn());
   }
+  
   public String startMenu() {
     System.out.println("Please choose an option below.");
     System.out.println("1. Login with your pin.");
@@ -64,22 +89,61 @@ public class ATM {
     System.out.println("Q. Quit the ATM.");
     return scan.next();
   }
+  
   public void createUser() {
     System.out.println("Please select one option below for bank accounts.");
     System.out.println("1. Checking Account ONLY");
     System.out.println("2. Saving Account ONLY");
     System.out.println("3. Checking & Saving Accounts");
     String choice = scan.next();
-    switch (choice) {
-      case "1":
-         this.user.add(new Users(new CheckingAccount(0f,0f,4.95f,999999999),"1111"));
-        break;
-      case "2":
-         this.user.add(new Users(new SavingsAccount(0f, 0.01f, 0f, 1),"1111"));
-      case "3":
-        this.user.add(new Users(new CheckingAccount(0f, 0f, 4.95f, 999999999),new SavingsAccount(0f, 0.01f, 0f, 1),
-            "1111"));
+    System.out.println("Please enter a 4 character pin for your account.");
+    String pinInput = scan.next();
+    if (pinInput.length() == 4) {
+      switch (choice) {
+        case "1":
+          this.user.add(new Users(new CheckingAccount(0f, 0f, 4.95f, 999999999), pinInput));
+          break;
+        case "2":
+          this.user.add(new Users(new SavingsAccount(0f, 0.01f, 0f, 1), pinInput));
+        case "3":
+          this.user.add(new Users(new CheckingAccount(0f, 0f, 4.95f, 999999999), new SavingsAccount(0f, 0.01f, 0f, 1),
+              pinInput));
+      }
     }
   }
   
+  public void accountPage() {
+    System.out.println("Welcome to your account page");
+    System.out.println("Please choose from an option below.");
+    System.out.println("1. Account Balance");
+    System.out.println("2. Deposit");
+    System.out.println("3. Withdraw");
+    String userInput = scan.next();
+    System.out.println(userInput);
+    switch (userInput) {
+      case "1" -> this.chooseAccount();
+      case "2" -> this.createUser();
+      case "q" -> {
+        System.out.println("Thanks for banking at Bank of Java! See you soon.");
+      }
+    }
+  }
+  
+  public String chooseAccount() {
+    if(this.getCurrentUser().getcAccount() != null && this.getCurrentUser().getsAccount() != null) {
+      System.out.println("Please pick between your two accounts");
+      System.out.println("1. Saving Account");
+      System.out.println("2. Checking Account");
+      return scan.next();
+    } else if (this.getCurrentUser().getcAccount() != null) {
+      System.out.println("You only have a Checking Account currently.");
+    } else {
+      System.out.println("You only have a Saving Account currently.");
+    }
+    return null;
+  }
+  
+  public void accountBalance() {
+    String userInput = this.chooseAccount();
+  }
 }
